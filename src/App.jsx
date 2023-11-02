2ba5b0aa6170d5358e1be28b78bd3c6a2074c8b6
mohammedasimahmed <asim60999@gmail.com>
2023-11-02 22:38:25 +0530

    added WhatsShopEase.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // import { Routes, Route } from "react-router-dom"
 import Navbar from "./Components/Navbar/Navbar.jsx"
+import WhatsShopEase from "./Components/WhatsShopEase.jsx"
 function App() {
 
   return (
     <>
       <Navbar />
+      <WhatsShopEase />
     </>
   )
 }
